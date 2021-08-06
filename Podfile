f9525dcace38133b2fea020a547b82b94358716d
Yaffi Azmi <yaffi@gredu.asia>
2021-08-06 16:39:21 +0700

    checkpoint: Unit Testing

--- Podfile
+++ Podfile
@@ -1,16 +1,21 @@
 # Uncomment the next line to define a global platform for your project
- platform :ios, '10.0'
+platform :ios, '10.0'
 
 target 'TMDB' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for TMDB
-
-pod 'Moya', '~> 14.0'
-pod 'RxSwift'
-pod 'RxCocoa'
-pod 'SwiftyJSON'
-pod 'Kingfisher'
-
+  
+  pod 'Moya', '~> 14.0'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'SwiftyJSON'
+  pod 'Kingfisher'
+  
+  target 'TMDBTests' do
+    pod 'Quick'
+    pod 'Nimble', '8.1.1'
+  end
+  
 end
--- TMDBTests/TMDBTests.swift
+++ TMDBTests/TMDBTests.swift
@@ -6,27 +6,108 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+import RxSwift
+@testable import TMDB
 
-class TMDBTests: XCTestCase {
+// MARK MovieList Unit Testing
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class MovieListControllerMock: MovieListDisplayLogic {
+    var router: MovieListRoutingLogic?
+    var isDidSuccessGetList = false
+    
+    func displayMovieListPopular(popularList: [MovieListViewModel.ViewModel.Popular]) {
+        isDidSuccessGetList = true
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+class MovieListInteractorMock: MovieListInteractorBusinessLogic {
+    var isGetMoviePopularList = false
+    func fetchMovieListPopular(page: Int) {
+        isGetMoviePopularList = true
     }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+class MovieListPresenterMock : MovieListPresentingLogic {
+    var isPresentSuccessGetList = false
+    func presentMovieListPopular(response: MovieListViewModel.Response.Movie) {
+        isPresentSuccessGetList = true
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+class MovieListWorkerMock: MovieListWorkerDelegate {
+    var isGetMoviePopularList = false
+    func getMoviePopular(_ page: Int) -> Observable<MovieListPopularEntity> {
+        isGetMoviePopularList = true
+    }
+}
+
+class TopListRouterMock: MovieListRoutingLogic {
+    var isNavigateToDetail = false
+    func navigateToPopularDetail(_ movieId: Int) {
+        isNavigateToDetail = true
+    }
+}
+
+
+class TopListTests: QuickSpec {
+    override func spec() {
+        describe("MovieListInteractor") {
+            var sut: MovieListInteractor!
+            var presenter: MovieListPresenterMock!
+            var worker: MovieListWorkerMock!
+            
+            beforeEach {
+                presenter = MovieListPresenterMock()
+                worker = MovieListWorkerMock()
+                let interactor = MovieListInteractor()
+                interactor.presenter = presenter
+                interactor.worker = worker
+                sut = interactor
+            }
+            
+            context("getMovieList called") {
+                it("When getMovieList is called, all the functions is called in getMovieList must be called succesfully") {
+                    sut.fetchMovieListPopular(page: 1)
+                    expect(worker.isGetMoviePopularList).to(beTrue())
+                }
+            }
+        }
+        
+        describe("MovieListPresenter") {
+            var sut: MovieListPresenter!
+            var view: MovieListControllerMock!
+
+            beforeEach {
+                view = MovieListControllerMock()
+                sut = MovieListPresenter()
+            }
+        }
+
+        describe("TopListRouter") {
+            var sut: MovieListRouter!
+            var view: MovieListViewController!
+
+            beforeEach {
+                view = MovieListViewController()
+                sut = MovieListRouter()
+            }
+
+            context("navigateToNews called") {
+                it("when navigateToNews function is called ") {
+                    sut.navigateToPopularDetail(1)
+                }
+            }
+        }
+
+        describe("MovieListWorker") {
+            context("MovieData") {
+                it("when data is checking") {
+                }
+            }
+
         }
     }
-
 }
+
